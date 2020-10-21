77fbc9b6365f36ee821c3fbcaa20b7ed7f1ba7d2
Seance <siddharth.dhananjay@gmail.com>
2020-10-21 15:09:06 +1100

    added html route paths

--- Unsolved/db/db.js
+++ Unsolved/db/db.js
@@ -6,5 +6,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 let notesarray = [];
-
-
+// html routes
+app.get('/notes', function(req, res){
+    res.sendfile(path.join(__dirname, "notes.html"));
+});
+app.get('*', function(req, res){
+    res.sendfile(path.join(__dirname, "index.html"));
+});
